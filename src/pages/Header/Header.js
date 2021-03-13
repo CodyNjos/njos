cbf32de3203c5b8bb85e536a1b42a9530f04e759
Cody Njos <cody.njos@live.com>
2021-03-12 22:04:00 -0600

    inital commit

---
+++ src/pages/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Styles/Header.css'
+function Header() {
+    return(
+        <div className = "header">
+        <p>Aasdf</p>
+        </div>
+    )
+}
+export default Header
