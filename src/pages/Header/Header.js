7c01c209ab3d93d44d9f689f32527a9fdc3a1b37
Cody Njos <cody.njos@live.com>
2021-03-12 23:48:56 -0600

    photo src:

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import '../Styles/Header.css'
+import {Button} from "@material-ui/core"
 function Header() {
     return(
         <div className = "header">
-        <p>Aasdf</p>
+        <h2>Hi, I'm Cody Njos</h2>
+        <h3>Software Developer</h3>
+        <h3>Game Enthusiest</h3>
+        <h3>Cat Dad</h3>
+        <img src = "../src/images/Cody_2.png"/>
         </div>
     )
 }
