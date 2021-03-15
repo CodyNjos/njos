6d659899252767f25d159c655f2ee54012671010
Cody Njos <cody.njos@live.com>
2021-03-15 00:14:43 -0500

    m

--- src/pages/Body/Body.js
+++ src/pages/Body/Body.js
@@ -1,24 +1,53 @@
 import React from 'react'
 import "../Styles/Body.css"
 import Fragment from "./Images/fragment.icon.png"
-function Body(params) {
-    return(
+import GitLogo from '../Header/Images/git.icon.png'
+import LinkInLogo from '../Header/Images/linkedin.icon.png'
+import Email from '../Header/Images/email.icon.png'
+function Body() {
+    return (
         <>
-        <div className = "main">
-            <div className="intro">
-            <h2>Intro About Me Filler</h2>
-            <p className="introBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi felis, cursus sed suscipit et, consequat nec nibh. Donec libero lorem, 
-                rhoncus id sollicitudin in, elementum at lacus. Nullam malesuada ullamcorper vehicula. Morbi ut consectetur enim. Quisque ac semper metus. 
-                Proin ac elit vel turpis lacinia semper non quis turpis. Pellentesque orci lectus, rhoncus in condimentum a, 
+            <div className="main">
+                <div className="intro">
+                    <h2 className="introHead">A Little About Me</h2>
+                    <p className="introBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi felis, cursus sed suscipit et, consequat nec nibh. Donec libero lorem,
+                    rhoncus id sollicitudin in, elementum at lacus. Nullam malesuada ullamcorper vehicula. Morbi ut consectetur enim. Quisque ac semper metus.
+                    Proin ac elit vel turpis lacinia semper non quis turpis. Pellentesque orci lectus, rhoncus in condimentum a,
                 blandit quis dolor. Duis nec scelerisque nunc, sit amet eleifend ex.</p>
+                </div>
+                <div className="links">
+                    <a href="https://github.com/CodyNjos" target="_blank" rel="noreferrer" >
+                        <img className="logoLink" src={GitLogo} alt="github" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/njos/" target="_blank" rel="noreferrer">
+                        <img className="logoLink" src={LinkInLogo} alt="linkedin" />
+                    </a>
+                    <a href="mailto:cody.njos@live.com" target="_blank" rel="noreferrer">
+                        <img className="logoLink" src={Email} alt="email" />
+                    </a>
+                </div>
             </div>
-        </div>
-        <div className="contentWrap">
-        <div className ="content">
-        <img className="iconHead" alt = "" src={Fragment}/>
-        <h3> Full Stack Developer</h3>
-        </div>
-        </div>
+            <div className="contentWrap">
+                <div className="content">
+                    <img className="iconHead" alt="" src={Fragment} />
+                    <h2 className="introHead"> Full Stack Developer</h2>
+                    <h2>
+                        Tools Ive Used
+                    </h2>
+                    <p className="introBody"> React JS</p>
+                    <p className="introBody">Node</p>
+                    <p className="introBody">Express</p>
+                    <p className="introBody">Postgres</p>
+                    <p className="introBody">Material UI</p>
+                    <p className="introBody">GitHub</p>
+                </div>
+            </div>
+            <h2 className="introHead">Some of My Recent Work</h2>
+            <div className="work">
+                <div className="projectCard">IMAGE + DESCTIPTION OF GAME EXCHANGE</div>
+                <div className="projectCard">IMAGE + DESCRIPTION OF SALARY CALCULATOR</div>
+                <div className="projectCard">IMAGE + DESCRIPTION OF CLIENT PROJECT</div>
+            </div>
         </>
     )
 }
--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,34 +1,24 @@
 import React from 'react'
 import '../Styles/Header.css'
-//import GitLogo from '../Header/Images/git.icon.png'
-import LinkInLogo from '../Header/Images/linkedin.icon.png'
+
 import HeadShot from '../Header/Images/Cody_2.png'
-import Email from '../Header/Images/email.icon.png'
+
+import Beetle from '../Header/Images/Lamp.png'
 function Header() {
-    const GitLogo = require('../Header/Images/git.icon.png-')
-    return (
+    return ( 
         <div className="header">
+
             <div className="descriptors">
             <div>
-                <h1>Hi, Im Cody Njos</h1>
-                <h3>I'm a Full Stack Developer Based in Fargo, North Dakota.</h3>
+                <h1 className="hi">Hi, I'm Cody Njos.</h1>
+                <h3 className="me">I'm a Full Stack Developer Based in Fargo, North Dakota.</h3>
             </div>
             </div>
              <div className="portraitWrap">
                 <img className="portrait" src={HeadShot} alt="borked" />
             </div>
-            <div className="links">
-                <a href="https://github.com/CodyNjos" target="_blank" rel="noreferrer" >
-                <img className = "logoLink" src={GitLogo} alt="github" />
-                </a>
-                <a href="https://www.linkedin.com/in/njos/" target="_blank" rel="noreferrer">
-                <img className = "logoLink" src ={LinkInLogo} alt="linkedin"/>
-                </a>
-                <a href="mailto:cody.njos@live.com" target="_blank" rel="noreferrer">
-                <img className = "logoLink" src ={Email} alt="email"/> 
-                </a>
-            </div>
-           <h1>A COOL AND FUN YELLOW GRAPHIC</h1>
+           
+           <img className="beetle" src ={Beetle} alt = ""/>
         </div>
     )
 }
